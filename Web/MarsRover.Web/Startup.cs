a55280a54cbb797796d04a51f68b459b72c4849d
Zhayne Chong Soriano <zhayne.soriano@live.com.ph>
2021-01-12 08:02:53 +0800

    Added Angular project with backend setup

---
+++ Web/MarsRover.Web/Startup.cs
@@ -0,0 +1,92 @@
+using MarsRover.Config.Models;
+using MarsRover.Core.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MarsRover.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var marsRoverClientConfig = new HttpClientConfig();
+            Configuration.GetSection("MarsRover").Bind(marsRoverClientConfig);
+
+            var marsRoverApiKeyConfig = new ApiKeyConfig();
+            Configuration.GetSection("Auth:MarsRover").Bind(marsRoverApiKeyConfig);
+
+            services.AddSingleton(marsRoverApiKeyConfig);
+
+            services.AddHttpClient<IMarsRoverClient, MarsRoverClient>(
+                client =>
+                {
+                    client.BaseAddress = marsRoverClientConfig.Url;
+                    client.DefaultRequestHeaders.Add("Accept", "application/json");
+                });
+            //TODO: Add retry
+
+            services.AddScoped<IFileService, FileService>();
+            services.AddScoped<IImageService, ImageService>();
+
+            services.AddControllersWithViews();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+    }
+}
---
+++ Web/MarsRover.Web/ViewModels/DatedImagesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarsRover.Web.ViewModels
+{
+    public class DatedImagesViewModel
+    {
+        public DateTime TakenOn { get; set; }
+        public IEnumerable<string> ImageUri { get; set; }
+    }
+}
